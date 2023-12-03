bcdec4382d41bbd6a5bc9cad852129ffff01aca7
AndriyKap <andriykap97@gmail.com>
2023-12-03 19:12:06 +0200

    Main navigation, DataGrid with search

--- FlowMeterTeamProject/Main.xaml.cs
+++ FlowMeterTeamProject/Main.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlowMeterTeamProject.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,12 +16,39 @@ using System.Windows.Shapes;
 
 namespace FlowMeterTeamProject
 {
-    
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Main.NavigationService.Navigate(new FlowMeterTeamProject.Pages.Houses());
+        }
+
+        private void BtnClick1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.Houses();
+        }
+        private void BtnClick2(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.PersonalAccounts();
+        }
+        private void BtnClick3(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.Counters();
+        }
+        private void BtnClick4(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.Services();
+        }
+        private void BtnClick5(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.Payments();
+        }
+        private void BtnClick6(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new FlowMeterTeamProject.Pages.Receipts();
         }
     }
 }
