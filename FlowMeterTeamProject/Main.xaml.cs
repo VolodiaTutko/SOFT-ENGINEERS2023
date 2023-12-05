963c546f559c1a74279daa3cb698240b70fd7083
namorone <roman.25117@gmail.com>
2023-12-06 00:27:10 +0200

    add login

--- FlowMeterTeamProject/Main.xaml.cs
+++ FlowMeterTeamProject/Main.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FlowMeterTeamProject.Data;
 
 namespace FlowMeterTeamProject
 {
@@ -26,8 +27,96 @@ namespace FlowMeterTeamProject
 
         private void SwitchToMain_Click(object sender, RoutedEventArgs e)
         {
-            MainContentControl.Content = new MainContent();
+            //// Отримання доступу до TextBoxLogin та TextBoxPassword
+            //TextBox textBoxLogin = Log_in.FindName("textlogin") as TextBox;
+            //TextBox textBoxPassword = Log_in.FindName("textpassword") as TextBox;
+
+
+            //if (textBoxLogin != null && textBoxPassword != null)
+            //{
+           // Отримання даних з TextBoxLogin та TextBoxPassword
+            string login = textlogin.Text;
+            string password = textpassword.Text;
+            
+            using (var dbContext = new AppDbContext())
+            {
+                var employee = dbContext.employees.FirstOrDefault(e => e.EmployeeLogin == login);
+                if (employee != null && employee.EmployeePassword == password)
+                {
+                    //MainLoginControler.Content = new MainContent();
+                    Window newWindow = new Window
+                    {
+                        Content = new MainContent(),
+                        Title = "FlowMeter"
+                    };
+
+                    // Show the new window
+                    Window.GetWindow(this).Close();
+                    newWindow.Show();
+                }
+                else
+                {
+                    // Invalid user, handle accordingly
+                }
+            }
+            
+
+            // Виведення даних в консоль (можна замінити на вашу логіку)
+            Console.WriteLine("Login: " + login);
+            Console.WriteLine("Password: " + password);
+            //}
+            //MainLoginControler.Content = new MainContent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+               
+                DragMove();
+            }
         }
 
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void TextBoxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            TextBlock backgroundText = (TextBlock)textBox.Template.FindName("BackgroundText", textBox);
+
+            if (textBox.Text.Length == 0 && !textBox.IsFocused)
+            {
+                backgroundText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                backgroundText.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
