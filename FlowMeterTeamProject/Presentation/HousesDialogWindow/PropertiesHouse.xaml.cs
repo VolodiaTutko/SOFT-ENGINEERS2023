954117de6e08513024f408993155448ab6001ab0
VolodiaTutko <volodatutko32@gmail.com>
2023-12-12 00:50:16 +0200

    add method for edit Accounts, and delete Houses

--- FlowMeterTeamProject/Presentation/HousesDialogWindow/PropertiesHouse.xaml.cs
+++ FlowMeterTeamProject/Presentation/HousesDialogWindow/PropertiesHouse.xaml.cs
@@ -1,5 +1,8 @@
 ﻿namespace Presentation.HousesDialogWindow
 {
+    using DAL.Data;
+    using FlowMeterTeamProject.Presentation;
+    using Presentation.Pages;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -19,10 +22,16 @@
     /// </summary>
     public partial class PropertiesHouse : Window
     {
-        public PropertiesHouse()
+
+        public int houseID;
+
+        private IDataGridUpdater _dataGridUpdater;
+        public PropertiesHouse(int houseid, IDataGridUpdater dataGridUpdater)
         {
             this.InitializeComponent();
             this.ResizeMode = ResizeMode.NoResize;
+            houseID = houseid;
+            _dataGridUpdater = dataGridUpdater;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,5 +39,52 @@
             var editData = new EditDataHouse();
             editData.ShowDialog();
         }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new AppDbContext())
+                {
+                    var houseToDelete = context.houses.FirstOrDefault(c => c.HouseId == houseID);
+
+
+                    var consumerToDelete = context.consumers.FirstOrDefault(a => a.HouseId == houseID);
+
+                    var serviceToDelete = context.services.FirstOrDefault(b => b.HouseId == houseID);
+                    
+
+
+                    if (houseToDelete != null)
+                    {
+
+                        context.houses.Remove(houseToDelete);
+                    }
+
+                    if (consumerToDelete != null)
+                    {
+
+                        context.consumers.Remove(consumerToDelete);
+                    }
+
+                    if(serviceToDelete != null)
+                    {
+
+                        context.services.Remove(serviceToDelete);
+                    }
+
+
+                    context.SaveChanges();
+                   this._dataGridUpdater?.UpdateDataGrid();
+                    this.Close();
+                    MessageBox.Show($"Житловий будинок : {houseToDelete.HouseAddress} видалено!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при видаленні даних: {ex.Message}", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
     }
 }
