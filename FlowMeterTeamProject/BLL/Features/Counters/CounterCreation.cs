00a629c8ceb0e8f55b93978c0ad5c61f465f78fb
ishandry <andriy.ishchukk@gmail.com>
2023-12-12 13:53:27 +0200

    counters history with resolve

--- FlowMeterTeamProject/BLL/Features/Counters/CounterCreation.cs
+++ FlowMeterTeamProject/BLL/Features/Counters/CounterCreation.cs
@@ -9,83 +9,30 @@ using FlowMeterTeamProject.DAL.DataServices;
 
 using FlowMeterTeamProject.BLL.Features.Consumers;
 using System.Reflection;
+using FlowMeterTeamProject.BLL.Features.Houses;
 
 namespace FlowMeterTeamProject.BLL.Features.Counters
 {
-    class CounterCreation
+    class CounterCreation : CounterRecordAdding
     {
-        private string consumerStr;
-        private Consumer consumer;
-        private string typeOfAccount;
-        private DateTime date;
-        private decimal indicator;
-        private ConsumersInfo consumersInfo;
-
-        public CounterCreation() {
-            this.consumerStr = string.Empty;
-            this.typeOfAccount = string.Empty;
-            this.consumersInfo = new ConsumersInfo();
-        }
-
-        public void SetConsumer(string consumer)
-        {
-            this.consumerStr = consumer;
-            this.consumer = consumersInfo.GetConsumerByOwner(consumer);
-        }
-
-        public void SetTypeOfAccount(string type)
-        {
-            this.typeOfAccount = type;
-        }
-
-        public void SetDate(DateTime dt)
-        {
-            this.date = dt;
-        }
-
-        public void SetIndicator(decimal indicator)
-        {
-            this.indicator = indicator;
-        }
-
-        public List<string> GetAvailableConsumers()
-        {
-            //todo: get all consumers
-            List<string> available = new List<string>();
-            foreach (var consumer in consumersInfo.GetAllConsumers()) {
-                available.Add(consumer.ConsumerOwner);
+        override public List<string> GetOperableServices() {
+            List<string> services = new List<string> { };
+            if (consumer != null) {
+                List<string> all = consumersInfo.GetConsumerServiceTypes(consumer);
+                List<string> withCounters = consumersInfo.GetServiceTypesWithCounters(consumer);
+                services = all.Except(withCounters).ToList();
             }
-            return available;
+            return services;
         }
 
-        public List<string> GetConsumerServices() {
-            return consumersInfo.GetConsumerServiceTypes(consumer);
-        }
-
-        public Boolean IsReadyToCreate() {
-            if (this.consumerStr == string.Empty
-                || this.typeOfAccount == string.Empty
-                || this.indicator == null
-                || this.date == null) {
-                return false;
-            } else
+        override public List<string> GetUnoperableServices()
+        {
+            List<string> services = new List<string> { };
+            if (consumer != null)
             {
-                return true;
+                services = consumersInfo.GetServiceTypesWithCounters(consumer);
             }
-        }
-
-        public void Create() {
-            if (!IsReadyToCreate())
-            {
-                throw new Exception("not ready to create");
-            }
-            else
-            {
-                var account = ConsumerDataService.GetConsumerAccount(consumer);
-                PropertyInfo propertyInfo = account.GetType().GetProperty(typeOfAccount);
-                string ServiceTypeAccount = (string)propertyInfo.GetValue(account);
-                CounterRecords.AddNewCounter(indicator, ServiceTypeAccount, typeOfAccount, date);
-            }
+            return services;
         }
     }
 }
---
+++ FlowMeterTeamProject/BLL/Features/Houses/HouseService.cs
@@ -0,0 +1,33 @@
+﻿using FlowMeterTeamProject.DAL.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Data;
+using FlowMeterTeamProject.BLL.Features.Consumers;
+using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
+
+namespace FlowMeterTeamProject.BLL.Features.Houses
+{
+    class HouseService
+    {
+        private readonly HouseRepository _houseRepository;
+
+        public HouseService() {
+            _houseRepository = new HouseRepository();
+        }
+
+        public List<string> GetAllHouseAddresses()
+        {
+            return _houseRepository.GetAll().Select(h => h.HouseAddress).ToList();
+        }
+
+        public List<Consumer> GetHouseConsumersByAddress(string address)
+        {
+            int houseId = _houseRepository.GetByAddress(address).HouseId;
+            return ConsumerDataService.GetConsumersByHouseId(houseId);
+        }
+    }
+}
